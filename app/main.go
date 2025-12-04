03dd52508d83583821d18e9659087d485a4e1ae9
Rayyan Khan <rayyanrehman101@gmail.com>
2025-12-04 18:55:23 +0530

    url parsing done

--- app/main.go
+++ app/main.go
@@ -4,25 +4,40 @@ import (
 	"fmt"
 	"net"
 	"os"
+	"strings"
 )
 
-
+// when /<random> write 404 status line, else if / then write 200 status line
 
 func main() {
 	fmt.Println("Logs from your program will appear here!")
-	
+
 	l, err := net.Listen("tcp", "0.0.0.0:4221")
 	if err != nil {
 		fmt.Println("Failed to bind to port 4221")
 		os.Exit(1)
 	}
 	defer l.Close()
-	
+
 	conn, err := l.Accept()
 	if err != nil {
 		fmt.Println("Error accepting connection: ", err.Error())
 		os.Exit(1)
 	}
-	status_line:="HTTP/1.1 200 OK\r\n\r\n"
-	conn.Write([]byte(status_line))
+	buffer := make([]byte, 1024)
+	_, err = conn.Read(buffer)
+	if err != nil {
+		fmt.Println("Error accepting connection: ", err.Error())
+		os.Exit(1)
+	}
+	req := string(buffer)
+	req_splitted := strings.Split(req, "\r\n")
+	req_line := strings.Split(req_splitted[0], " ")
+	url := req_line[1]
+	if url == "/" {
+		conn.Write([]byte("HTTP/1.1 200 OK\r\n\r\n"))
+	} else {
+		conn.Write([]byte("HTTP/1.1 404 Not Found\r\n\r\n"))
+	}
+
 }
