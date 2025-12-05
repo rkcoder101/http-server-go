14b6b2f07328246ebdad7c58413f457e8705f3cf
Rayyan Khan <rayyanrehman101@gmail.com>
2025-12-05 11:53:54 +0530

    sending response to the client

--- app/main.go
+++ app/main.go
@@ -7,8 +7,19 @@ import (
 	"strings"
 )
 
-// when /<random> write 404 status line, else if / then write 200 status line
-
+func url_parser(conn *net.Conn) string {
+	buffer := make([]byte, 1024)
+	_, err := (*conn).Read(buffer)
+	if err != nil {
+		fmt.Println("Error accepting connection: ", err.Error())
+		os.Exit(1)
+	}
+	req := string(buffer)
+	req_splitted := strings.Split(req, "\r\n")
+	req_line := strings.Split(req_splitted[0], " ")
+	url := req_line[1]
+	return url
+}
 func main() {
 	fmt.Println("Logs from your program will appear here!")
 
@@ -24,20 +35,10 @@ func main() {
 		fmt.Println("Error accepting connection: ", err.Error())
 		os.Exit(1)
 	}
-	buffer := make([]byte, 1024)
-	_, err = conn.Read(buffer)
-	if err != nil {
-		fmt.Println("Error accepting connection: ", err.Error())
-		os.Exit(1)
-	}
-	req := string(buffer)
-	req_splitted := strings.Split(req, "\r\n")
-	req_line := strings.Split(req_splitted[0], " ")
-	url := req_line[1]
-	if url == "/" {
-		conn.Write([]byte("HTTP/1.1 200 OK\r\n\r\n"))
-	} else {
-		conn.Write([]byte("HTTP/1.1 404 Not Found\r\n\r\n"))
-	}
-
+	// we will get hit by a GET req
+	// we need to write to the client, with headers and response body
+	url := url_parser(&conn)
+	str := url[6:]
+	res:=fmt.Sprintf("HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: %d\r\n\r\n%s",len(str),str)
+	conn.Write([]byte(res))
 }
