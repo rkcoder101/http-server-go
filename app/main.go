149707ce4947f4e6184d7ffbf87d30b5d8dead52
Rayyan Khan <rayyanrehman101@gmail.com>
2025-12-06 16:34:06 +0530

    sending user-agent at the /user-agent endpoint

--- app/main.go
+++ app/main.go
@@ -1,13 +1,14 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 	"net"
 	"os"
 	"strings"
 )
 
-func url_parser(conn *net.Conn) string {
+func req_parser(conn *net.Conn) string {
 	buffer := make([]byte, 1024)
 	_, err := (*conn).Read(buffer)
 	if err != nil {
@@ -15,10 +16,22 @@ func url_parser(conn *net.Conn) string {
 		os.Exit(1)
 	}
 	req := string(buffer)
+	return req
+}
+func url_parser(req string) string{
 	req_splitted := strings.Split(req, "\r\n")
 	req_line := strings.Split(req_splitted[0], " ")
 	url := req_line[1]
 	return url
+}
+func userAgent_parser(req string) (string, error) {	
+	req_splitted := strings.SplitSeq(req, "\r\n")
+	for v := range req_splitted {
+		if strings.HasPrefix(v, "User-Agent") {
+			return v[11:], nil
+		}
+	}
+	return "", errors.New("User-Agent not found")
 }
 func main() {
 	fmt.Println("Logs from your program will appear here!")
@@ -35,14 +48,20 @@ func main() {
 		fmt.Println("Error accepting connection: ", err.Error())
 		os.Exit(1)
 	}
-	// we will get hit by a GET req
-	// we need to write to the client, with headers and response body
-	url := url_parser(&conn)
+
+	req := req_parser(&conn)
+	url:=url_parser(req)
 	switch {
 	case url == "/":
 		conn.Write([]byte("HTTP/1.1 200 OK\r\n\r\n"))
-	case strings.HasPrefix(url,"/echo/"):
+	case strings.HasPrefix(url, "/echo/"):
 		conn.Write([]byte(fmt.Sprintf("HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: %d\r\n\r\n%s", len(url[6:]), url[6:])))
+	case strings.HasPrefix(url, "/user-agent"):
+		user_agent, _ := userAgent_parser(req)
+		// if err != nil {
+		// 	fmt.Println("lol errur aa gya bro")
+		// }
+		conn.Write([]byte(fmt.Sprintf("HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: %d\r\n\r\n%s", len(user_agent), user_agent)))
 	default:
 		conn.Write([]byte("HTTP/1.1 404 Not Found\r\n\r\n"))
 	}
